23f02cf08596c2895fb5cf428c087922679bc69c
jadeswift <jadefullness@gmail.com>
2021-04-29 01:12:41 -0500

    overlay, background, and logo

--- wp-content/themes/mightyHVACtheme/page-templates/template-home.php
+++ wp-content/themes/mightyHVACtheme/page-templates/template-home.php
@@ -11,8 +11,24 @@
 defined( 'ABSPATH' ) || exit;
 
 get_header();
-
 ?>
+<div class="row no-gutters">
+	<div class="col-lg-7 no-gutters">
+		<div class="leftside">
+			<div class="background">
+				<img id="mighty-logo" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/mighty-logo.png">
+				<img id="overlay" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/overlay.png">
+			</div>
+		</div>
+	</div>
+	<div class="col-lg-5 no-gutters">
+		<div class="rightside">
+			<div class="background">
+				<img id="mighty-logo" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/mighty-logo.png">
+				<img id="overlay" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/overlay.png">
+			</div>
+		</div>
+	</div>
+</div>
 
-<?php
-get_footer();
+<?php get_footer();
