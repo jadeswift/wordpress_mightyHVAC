a7272a6b3607190d0023da7f5be77c6d39488d55
jadeswift <jadefullness@gmail.com>
2021-05-04 09:34:25 -0500

    home page is finished.. forgot some commits along the way

--- wp-content/plugins/getwid/includes/templates/template-parts/post-meta.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//extract styles & classes
-extract($extra_attr);
-?>
-
-<div class="<?php echo esc_attr( $wrapper_class ); ?>" <?php echo (!empty($wrapper_style) ? 'style="'.esc_attr($wrapper_style).'"' : '');?>>
-    <?php echo $content; ?>
-</div>
--- wp-content/themes/mightyHVACtheme/page-templates/template-home.php
+++ wp-content/themes/mightyHVACtheme/page-templates/template-home.php
@@ -50,12 +50,147 @@ get_header();
 				</div>
 			</div>
 			<div class="half-the-battle">
+				<div class="image rectangle">
+				</div>
 				<div class="heading-text">
-					<p>
-						Half the battle is
+					<h1>
+						Half the battle is<br>
 						understanding<br>
-						the whole home.
+						<span>the whole home.</span>
+					</h1>
+					<p>
+						Home performance is the science of
+						understanding how a home works. Together,
+						we’ll work on designing a solution that’s
+						right for you.
 					</p>
+				</div>
+				<div class="info-rectangles row no-gutters">
+					<div class="col col-sm-6 no-gutters">
+						<div class="rectangle left"><p>Blower Door Test</p></div>
+					</div>
+					<div class="col col-sm-6 no-gutters">
+						<div class="rectangle right"><p>Thermal Imaging</p></div>
+					</div>
+				</div>
+				<div class="info-rectangles row no-gutters">
+					<div class="col col-sm-6 no-gutters">
+						<div class="rectangle left"><p>HVAC System Design</p></div>
+					</div>
+					<div class="col col-sm-6 no-gutters">
+						<div class="rectangle right"><p>Carbon Monoxide and Combustion Safety</p></div>
+					</div>
+				</div>
+				<div class="info-rectangles row no-gutters">
+					<div class="col col-sm-6 no-gutters">
+						<div class="rectangle left"><p>Home Energy Audit</p></div>
+					</div>
+					<div class="col col-sm-6 no-gutters">
+						<div class="rectangle right"><p>Home Comfort Optimization</p></div>
+					</div>
+				</div>
+				<div class="learn-more row">
+					<div class="col-sm-8">
+						<p>GET A WHOLE HOME EVALUATION</p>
+					</div>
+					<div class="col-sm-4">
+						<button type="button" class="btn">Learn More</button>
+					</div>
+				</div>
+			</div>
+			<div class="folks">
+				<div class="row">
+					<div id="folks-title" class="col-sm-8">
+						<h2>THESE FOLKS FEEL GOOD INSIDE</h2>
+					</div>
+					<div class="col-sm-4">
+						<button type="button" class="btn">Read More</button>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col col-sm-4">
+						<img class="person" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/Kelly.png">
+					</div>
+					<div class="col col-sm-8">
+						<img class="quote" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/quote.png">
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+							sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+							magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+						</p>
+						<p class="name">
+							Kelly T.
+						</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col col-sm-8">
+						<img class="quote" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/quote.png">
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+							sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+							magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+						</p>
+						<p class="name">
+							Andrew J.
+						</p>
+					</div>
+					<div class="col col-sm-4">
+						<img class="person" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/Andrew.png">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col col-sm-4">
+						<img class="person" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/Samantha.png">
+					</div>
+					<div class="col col-sm-8">
+						<img class="quote" src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/quote.png">
+						<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+							sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+							magna aliquam erat volutpat. Ut wisi enim ad minim veniam.
+						</p>
+						<p class="name">
+							Samantha R.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="iwaver">
+				<div class="row no-gutters">
+					<div class="col-sm-8 no-gutters">
+						<p id="price">$699</p>
+						<p id="description">For an iWave-R</p>
+						<p id="subtext">Get cleaner air with an iWave-R!</p>
+					</div>
+					<div class="col-sm-4 no-gutters">
+						<p>GET A GREAT DEAL!</p>
+						<button type="button" class="btn">More Deals</button>
+					</div>
+				</div>
+			</div>
+			<div class="maintenance">
+				<h1>Maintenance Plans</h1>
+				<div class="row no-gutters">
+					<div class="col-sm-8 no-gutters">
+						<div class="image rectangle">
+						</div>
+						<p>
+							The extreme Central Iowa temperatures put a lot of
+							wear and tear on your heating and cooling systems.
+							The best way to keep them running efficiently is to
+							schedule maintenance checkups. Our maintenance plan
+							is designed with three things in mind—keeping utility
+							costs down, comfort up, and making sure your system lasts.<br><br>
+							<span>We offer three different tiers with distinct benefits.</span>
+						</p>
+						<button type="button" class="btn">Learn More</button>
+					</div>
+					<div class="col-sm-4 no-gutters">
+						<img src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/basic.png"><br>
+						<img src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/smart.png"><br>
+						<img src="/wordpress-site/wp-content/themes/mightyHVACtheme/sass/assets/images/platinum.png"><br>
+					</div>
 				</div>
 			</div>
 		</div>
